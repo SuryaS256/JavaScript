994158d64c5e646c2c1472e0120db18524529b7e
Surya S <surya.smg256@gmail.com>
2024-11-15 10:40:33 +0530

    Add files via upload

---
+++ split_join.js
@@ -0,0 +1,10 @@
+//Split the sentence into an array of words and print array.
+let sentence = "I love JavaScript";
+let words = sentence.split(' ');
+console.log(words);
+
+//Join the array of words into a new string where each word is separated by a dash (-) and print string.
+
+let joined = words.join('-');
+console.log(joined); 
+
